c1a25619d6b747c4ff8b5b6e5aa864c09a49ef22
ChangJoon_Lee <kelvinjr177@icloud.com>
2023-01-02 10:16:36 +0900

    2nd commit

--- _01_lab_recursion/Program.cs
+++ _01_lab_recursion/Program.cs
@@ -10,6 +10,22 @@ namespace _01_lab_recursion
     {
         static void Main(string[] args)
         {
+            int n = 0;
+            string strNum = Console.ReadLine();
+            n = Int32.Parse(strNum);
+
+            HelloWorld(n);
+        }
+
+        public static void HelloWorld(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine("HelloWorld");
+            HelloWorld(n - 1);
         }
     }
 }
